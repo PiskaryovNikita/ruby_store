744389d3dc2ba17c65d37f4b7668860ab9ce7a3f
Mykyta Piskarov <Mykyta_Piskarov@epam.com>
2021-07-13 15:44:13 +0300

    Add callbacks for Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,10 @@ class Item < ApplicationRecord
   attr_accessor :price, :name, :real, :weight
 
   validates :price, {numericality: {greater_than: 0}}
+
+  after_initialize { puts "initialized" } #Item.new; Item.first
+  after_save {} #Item.save; Item.create; Item.update_attributes
+  after_create {} #Item.create
+  after_update {} #Item.update
+  after_destroy {} #Item.destroy
 end
