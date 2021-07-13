58eab8c3935b5b2b78d77c05f503f340893eaea9
Mykyta Piskarov <Mykyta_Piskarov@epam.com>
2021-07-13 15:58:52 +0300

    Add controller endpoint

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,12 @@
+class ItemsController < ApplicationController
+
+  def index
+    @items = Item.all
+    render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
+  end
+
+  def create
+    p params
+  end
+
+end
